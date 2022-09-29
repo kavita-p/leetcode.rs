e0a5f53ef286e8d7f18d3fbb3299bf85f9cb82a9
Kavita <kavi.pdri@gmail.com>
2022-09-29 16:09:10 -0700

    sep 29

--- src/problems.rs
+++ src/problems.rs
@@ -1,2 +1,3 @@
+mod find_k_closest_elements;
 mod reverse_words_in_string_iii;
 mod sum_even_after_queries;
---
+++ src/problems/find_k_closest_elements.rs
@@ -0,0 +1,34 @@
+// url: https://leetcode.com/problems/find-k-closest-elements/
+// date: 29 sep. 2022
+// adapted from forum solution
+
+
+pub fn find_closest_elements(arr: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
+    let k = k as usize;
+    let mut l = 0;
+    let mut r = arr.len() - k;
+    while l < r {
+        let mid = l + (r - l) / 2;
+        if x - arr[mid] > arr[mid + k] - x {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
+    }
+    arr[l..(l + k)].into()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn leetcode_e1 () {
+        let arr = vec![1, 2, 3, 4, 5];
+        let k = 4;
+        let x = 3;
+        let output = vec![1, 2, 3, 4];
+
+        assert_eq!(find_closest_elements(arr, k, x), output);
+    }
+}
