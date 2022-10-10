f40cc9dacd0fbd23a160a7ee5bc5a14ee3ef86b5
Kavita <kavi.pdri@gmail.com>
2022-10-10 13:18:01 -0700

    break_palindrome

--- src/problems.rs
+++ src/problems.rs
@@ -1,3 +1,4 @@
+mod break_palindrome;
 mod find_k_closest_elements;
 mod reverse_words_in_string_iii;
 mod sum_even_after_queries;
---
+++ src/problems/break_palindrome.rs
@@ -0,0 +1,36 @@
+// url: https://leetcode.com/problems/break-a-palindrome
+// date: 10 oct. 2022
+
+pub fn break_palindrome(palindrome: String) -> String {
+    if palindrome.len() <= 1 {
+        return String::from("");
+    }
+
+    let mut bytes = palindrome.as_bytes().to_vec();
+
+    if let Some(i) = (0..palindrome.len() / 2)
+        .into_iter()
+        .find(|&i| bytes[i] != b'a')
+    {
+        bytes[i] = b'a';
+    } else {
+        bytes[palindrome.len() - 1] = b'b';
+    }
+
+    String::from_utf8_lossy(&bytes).into_owned()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn leetcode_e1() {
+        assert_eq!(break_palindrome("abccba".to_string()), "aaccba".to_string())
+    }
+
+    #[test]
+    fn leetcode_e2() {
+        assert_eq!(break_palindrome("a".to_string()), "".to_string())
+    }
+}
