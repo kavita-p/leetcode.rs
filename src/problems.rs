b170e9502b4b1d9db634873e3dea0b0214263b6d
Kavita Poduri <kavi.pdri@gmail.com>
2022-10-18 12:50:40 -0700

    count and say

--- src/problems.rs
+++ src/problems.rs
@@ -1,6 +1,7 @@
 mod break_palindrome;
+mod check_if_pangram;
+mod count_and_say;
 mod find_k_closest_elements;
 mod reverse_words_in_string_iii;
 mod sum_even_after_queries;
 mod time_based_key_value_store;
-mod check_if_pangram;
---
+++ src/problems/count_and_say.rs
@@ -0,0 +1,47 @@
+// 18 oct. 2022
+// from https://leetcode.com/problems/count-and-say/
+
+struct Solution {}
+
+impl Solution {
+    pub fn count_and_say(n: i32) -> String {
+        if n == 1 {
+            return "1".to_string();
+        } else {
+            let mut say: Vec<char> = vec![];
+            let mut digit_count: u8 = '0' as u8;
+            let mut prev = ' ';
+            for char in Self::count_and_say(n - 1).chars().into_iter() {
+                if prev == ' ' {
+                    prev = char;
+                    digit_count = '1' as u8;
+                } else if char == prev {
+                    digit_count += 1;
+                } else {
+                    say.push(digit_count as char);
+                    say.push(prev);
+                    prev = char;
+                    digit_count = '1' as u8;
+                }
+            }
+            say.push(digit_count as char);
+            say.push(prev);
+            say.into_iter().collect()
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn leetcode_e1() {
+        assert_eq!(Solution::count_and_say(1), "1".to_string())
+    }
+
+    #[test]
+    fn leetcode_e2() {
+        assert_eq!(Solution::count_and_say(4), "1211".to_string())
+    }
+}
