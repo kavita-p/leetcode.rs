1c29dd950a88102cec91f7533a1766de2cabfdf8
Kavita Poduri <kavi.pdri@gmail.com>
2022-10-19 12:17:34 -0700

    top_k_frequent_words

--- src/problems.rs
+++ src/problems.rs
@@ -5,3 +5,4 @@ mod find_k_closest_elements;
 mod reverse_words_in_string_iii;
 mod sum_even_after_queries;
 mod time_based_key_value_store;
+mod top_k_frequent_words;
---
+++ src/problems/top_k_frequent_words.rs
@@ -0,0 +1,51 @@
+// 19 oct. 2022
+// from https://leetcode.com/problems/top-k-frequent-words/
+// adapted from forum solution
+
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+    pub fn top_k_frequent(words: Vec<String>, k: i32) -> Vec<String> {
+        // vector of (frequency, word) tuples
+        let mut word_counts: Vec<(i32, String)> = words
+            .into_iter()
+            .fold(HashMap::new(), |mut counts, word| {
+                *counts.entry(word).or_insert(0) += 1;
+                counts
+            })
+            .into_iter()
+            // make counts negative for easy sorting
+            .map(|(w, count)| (-count, w))
+            .collect();
+
+        word_counts.sort();
+
+        word_counts
+            .into_iter()
+            // drop counts
+            .map(|(_count, w)| w)
+            // :)
+            .take(k as usize)
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn leetcode_e1() {
+        let words = ["i", "love", "leetcode", "i", "love", "coding"]
+            .map(String::from)
+            .to_vec();
+        let k = 2;
+
+        assert_eq!(
+            Solution::top_k_frequent(words, k),
+            ["i", "love"].map(String::from).to_vec()
+        )
+    }
+}
