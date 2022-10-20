a9bc2a404f1ccf2647f922b95d39252e4b6eb67a
Kavita Poduri <kavi.pdri@gmail.com>
2022-10-20 10:57:16 -0700

    int_to_roman

--- src/problems.rs
+++ src/problems.rs
@@ -2,6 +2,7 @@ mod break_palindrome;
 mod check_if_pangram;
 mod count_and_say;
 mod find_k_closest_elements;
+mod int_to_roman;
 mod reverse_words_in_string_iii;
 mod sum_even_after_queries;
 mod time_based_key_value_store;
---
+++ src/problems/int_to_roman.rs
@@ -0,0 +1,53 @@
+// 20 oct. 2022
+// from https://leetcode.com/problems/integer-to-roman/
+
+struct Solution;
+
+impl Solution {
+    pub fn int_to_roman(mut num: i32) -> String {
+        let dict: Vec<(i32, &str)> = vec![
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ];
+        let mut numerus = "".to_string();
+        for &(value, roman) in dict.iter() {
+            let roman_digits = num / value;
+            if roman_digits > 0 {
+                numerus.push_str(&roman.repeat(roman_digits as usize));
+                num = num % value;
+            }
+        }
+        numerus
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn leetcode_e1() {
+        assert_eq!(Solution::int_to_roman(3), "III".to_string())
+    }
+
+    #[test]
+    fn leetcode_e2() {
+        assert_eq!(Solution::int_to_roman(58), "LVIII".to_string())
+    }
+
+    #[test]
+    fn leetcode_e3() {
+        assert_eq!(Solution::int_to_roman(1994), "MCMXCIV".to_string())
+    }
+}
